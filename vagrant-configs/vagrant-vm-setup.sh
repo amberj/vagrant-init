746b757cfe586b13cbbe545f108b129b0385fe18
Amber Jain <i.amber.jain@gmail.com>
2013-09-23 00:21:30 +0530

    added the code using which user can easily add one-time-only initial configuration/setup commands inside VM when the VM is booted for the very first time

--- vagrant-configs/vagrant-vm-setup.sh
+++ vagrant-configs/vagrant-vm-setup.sh
@@ -4,5 +4,19 @@
 # executed. Put any commands in this script to setup your VM according to
 # your needs. For example:
 
+echo "==========================================================="
+echo "[vagrant-vm-setup.sh] Setting up vagrant VM, please wait..."
+echo "==========================================================="
+
 sudo apt-get install hello
 
+# Any commands inside the 'if' decision construct below are run only the
+# very first time VM is booted using 'vagrant up'. You can use this to
+# do one-time-only configuration to setup the VM.
+if [ ! -f /home/vagrant/.vagrant_do_not_delete ]; then
+    # Add one-time-only configuration/setup commands here. For e.g.:
+    echo "Configuring the VM to make it ready to use..."
+
+    # Do NOT delete the following line:
+    touch /home/vagrant/.vagrant_do_not_delete
+fi
