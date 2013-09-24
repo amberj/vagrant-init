37ef960779e3317cc4c345a8daca74001059a898
Amber Jain <i.amber.jain@gmail.com>
2013-09-24 09:46:54 +0530

    On second thought, apt sources should only the very first time VM is setup.
    In future, it's upto the user to update apt sources whenever he/she adds
    new repos.

--- vagrant-configs/vagrant-vm-setup.sh
+++ vagrant-configs/vagrant-vm-setup.sh
@@ -8,16 +8,15 @@ echo "==========================================================="
 echo "[vagrant-vm-setup.sh] Setting up vagrant VM, please wait..."
 echo "==========================================================="
 
-sudo apt-get update
-#sudo apt-get install hello
-
 # Any commands inside the 'if' decision construct below are run only the
 # very first time VM is booted using 'vagrant up'. You can use this to
 # do one-time-only configuration to setup the VM.
 if [ ! -f /home/vagrant/.vagrant_do_not_delete ]; then
     # Add one-time-only configuration/setup commands here. For e.g.:
     echo "Configuring the VM to make it ready to use..."
-
+    sudo apt-get -y update
+    #sudo apt-get -y install hello
+    
     # Do NOT delete the following line:
     touch /home/vagrant/.vagrant_do_not_delete
 fi
